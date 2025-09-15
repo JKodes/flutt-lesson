de313f46e1c5de47608b2978c2285cc5c37176b2
JKodes <adrainford09@gmail.com>
2025-09-15 16:01:19 -0400

    made validator for input fields

--- lib/main.dart
+++ lib/main.dart
@@ -31,25 +31,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<FormState> _signInKey = GlobalKey();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+    r"^[a-zA0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFEFEFA),
       appBar: AppBar(title: Text("Hello")),
       body: Form(
+        key: _signInKey,
         child: Column(
           children: <Widget>[
             TextFormField(
               controller: _emailController,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(hintText: "Enter an Email"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter an email";
+                } else if (emailValid.hasMatch(value)) {}
+                return null;
+              },
             ), //email
             TextFormField(
               controller: _passwordController,
               obscureText: true,
               decoration: InputDecoration(hintText: "Enter a Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a password";
+                } else if (value.length > 6) {
+                  return "Password must be atleast 6 characters";
+                }
+                return null;
+              },
             ), //password
             ElevatedButton(
               onPressed: () {
